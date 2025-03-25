7cd7223b1c3699285926b30b778a3369be9d80ef
David Auresy Serrano <32300423+JosephAuresy@users.noreply.github.com>
2025-03-25 15:10:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ import io
 # Set the title and favicon that appear in the browser's tab bar.
 st.set_page_config(
     page_title='Xwulqwselu',
-    page_icon=':mountain:',
+    page_icon=':snow_capped_mountain:',
 )
 
 # Sidebar for navigation
